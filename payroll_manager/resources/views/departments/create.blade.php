66edb0bf69bbbe4d0ece39059cf626fdf2339e2d
Yahuza Nuhu <87288867+nuhuyahuza@users.noreply.github.com>
2023-02-08 16:10:30 +0000

    corrections made to files

---
+++ payroll_manager/resources/views/departments/create.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+    <div class="main-content container-fluid">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Manage Department</h3>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class='breadcrumb-header'>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html" class="text-success">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Manage Department</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <section class="section">
+            <div class="card">
+                <div class="card-body">
+                    <table class='table' id="table1">
+                        <thead>
+                            <tr>
+                                <th>Department Name</th>
+                                <th>Department Short Name</th>
+                                <th>Creation Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </x-layout>
