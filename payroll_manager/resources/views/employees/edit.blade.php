8d606d8efb792cba9edfaebc9d71953556a5782c
Yahuza Nuhu <87288867+nuhuyahuza@users.noreply.github.com>
2023-02-12 00:13:33 +0000

    employee component

---
+++ payroll_manager/resources/views/employees/edit.blade.php
@@ -0,0 +1,191 @@
+<x-layout>
+    <div class="main-content container-fluid">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Add Employee</h3>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class='breadcrumb-header'>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html" class="text-success">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Add Employee</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+    
+        </div>
+    
+    
+        <!-- // Basic multiple Column Form section start -->
+        <section id="multiple-column-form">
+            <div class="row match-height">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                <form class="form" method="POST" action="/employeesf.sswewe">
+                                    <div class="row">
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">ID Number</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="id number" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-hash"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Gender</label>
+                                                <div class="position-relative">
+                                                    <fieldset class="form-group">
+                                                        <select class="form-select" id="basicSelect">
+                                                            <option>Male</option>
+                                                            <option>Female</option>
+                                                        </select>
+                                                    </fieldset>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">First Name</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="first name" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Middle Name</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="middle name" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div><div class="col-md-4 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Last Name</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="last name" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Age</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="age" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Email</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="email" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-envelope"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Contact</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="contact" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-phone"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Profile</label>
+                                                <div class="position-relative">
+                                                    <input type="file" class="form-control" placeholder="" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group">
+                                                <label for="country-floating">Deapartment</label>
+                                                    <fieldset class="form-group">
+                                                        <select class="form-select" id="basicSelect">
+                                                            <option>IT</option>
+                                                            <option>ENGINEERING</option>
+                                                            <option>HR</option>
+                                                            <option>FINANCE</option>
+                                                        </select>
+                                                    </fieldset>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group">
+                                                <label for="company-column">Designation</label>
+                                                    <fieldset class="form-group">
+                                                        <select class="form-select" id="basicSelect">
+                                                            <option>IT</option>
+                                                            <option>MANAGER</option>
+                                                            <option>SUPERVISOR</option>
+                                                            <option>ENGINEER</option>
+                                                        </select>
+                                                    </fieldset>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Username</label>
+                                                <div class="position-relative">
+                                                    <input type="text" class="form-control" placeholder="username" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Password</label>
+                                                <div class="position-relative">
+                                                    <input type="password" class="form-control" placeholder="passsword" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="fa fa-key"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- // Basic multiple Column Form section end -->
+    </div>
+</x-layout>
