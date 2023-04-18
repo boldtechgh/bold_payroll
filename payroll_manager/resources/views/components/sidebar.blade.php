5419c3a0d07434cbf8dcb51bee6553cb85ee1f1d
devMozay <mosesanao13@gmail.com>
2023-04-18 12:10:41 +0000

    Minor updates to Organization

--- payroll_manager/resources/views/components/sidebar.blade.php
+++ payroll_manager/resources/views/components/sidebar.blade.php
@@ -1,44 +1,44 @@
 <div id="sidebar" class='active'>
     <div class="sidebar-wrapper active">
-      <div class="sidebar-header text-white">
-              <i class="fa fa-money-bill me-4"></i>
-                <span>PAYROLL</span>
-      </div>
-       <div class="sidebar-menu">
-          <ul class="menu">
-             <li class="sidebar-item">
-                <a href="/" class='sidebar-link'>
-                <i class="fa fa-home text-success"></i>
-                <span>Dashboard</span>
-                </a>
-             </li>
-             <li class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                <i class="fa fa-building text-success"></i>
-                <span>Organization</span>
-                </a>
-                <ul class="submenu ">
-                <li>
-                      <a href="/organizations/create">Add Organization Info</a>
-                   </li>
-                   <li>
-                      <a href="/organizations">Manage Organization Info</a>
-                   </li>
-                   <li>
-                      <a href="/departments/create">Add Department</a>
-                   </li>
-                   <li>
-                      <a href="/departments">Manage Departments</a>
-                   </li>
-                    <li>
-                      <a href="/designations/create">Add Designation</a>
-                   </li>
-                   <li>
-                      <a href="/designations">Manage Designations</a>
-                   </li>
-                </ul>
-             </li>
-             {{-- <li class="sidebar-item  has-sub">
+        <div class="sidebar-header text-white">
+            <i class="fa fa-money-bill me-4"></i>
+            <span>PAYROLL</span>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-item">
+                    <a href="/" class='sidebar-link'>
+                        <i class="fa fa-home text-success"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="fa fa-building text-success"></i>
+                        <span>Organization</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li>
+                            <a href="/organizations/create">Add Organization</a>
+                        </li>
+                        <li>
+                            <a href="/organizations">Manage Organization</a>
+                        </li>
+                        <li>
+                            <a href="/departments/create">Add Department</a>
+                        </li>
+                        <li>
+                            <a href="/departments">Manage Departments</a>
+                        </li>
+                        <li>
+                            <a href="/designations/create">Add Designation</a>
+                        </li>
+                        <li>
+                            <a href="/designations">Manage Designations</a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- <li class="sidebar-item  has-sub">
                 <a href="#" class='sidebar-link'>
                 <i class="fa fa-table text-success"></i>
                 <span>Designation</span>
@@ -53,27 +53,27 @@
                 </ul>
              </li> --}}
 
-             <li class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                <i class="fa fa-users text-success"></i>
-                <span>Employees</span>
-                </a>
-                <ul class="submenu ">
-                   <li>
-                      <a href="/employees/create">Add Employee</a>
-                   </li>
-                   <li>
-                      <a href="/employees">Manage Employees</a>
-                   </li>
-                   <li>
-                    <a href="attendance">Record Attendance</a>
-                  </li>
-                  <li>
-                    <a href="view_attendance">View Attendance List</a>
-                  </li>
-                </ul>
-             </li>
-             {{-- <li class="sidebar-item has-sub">
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="fa fa-users text-success"></i>
+                        <span>Employees</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li>
+                            <a href="/employees/create">Add Employee</a>
+                        </li>
+                        <li>
+                            <a href="/employees">Manage Employees</a>
+                        </li>
+                        <li>
+                            <a href="attendance">Record Attendance</a>
+                        </li>
+                        <li>
+                            <a href="view_attendance">View Attendance List</a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- <li class="sidebar-item has-sub">
                 <a href="#" class="sidebar-link">
                   <i class="fa fa-building text-success"></i>
                   <span>Attendance</span>
@@ -88,7 +88,7 @@
                 </ul>
               </li>
              </li> --}}
-             {{-- <li class="sidebar-item has-sub">
+                {{-- <li class="sidebar-item has-sub">
                 <a href="#" class="sidebar-link">
                   <i class="fa fa-building text-success"></i>
                   <span>Allowances</span>
@@ -103,7 +103,7 @@
                 </ul>
               </li>
              </li> --}}
-             {{-- <li class="sidebar-item has-sub">
+                {{-- <li class="sidebar-item has-sub">
                 <a href="#" class="sidebar-link">
                   <i class="fa fa-building text-success"></i>
                   <span>Deductions</span>
@@ -118,66 +118,66 @@
                 </ul>
               </li>
              </li> --}}
-             <li class="sidebar-item has-sub">
-                <a href="#" class="sidebar-link">
-                  <i class="fa fa-building text-success"></i>
-                  <span>Allowances</span>
-                </a>
-                <ul class="submenu">
-                  <li>
-                  <a href="/allowances/create">Add Allowance</a>
-                  </li>
-                  <li>
-                  <a href="/allowances">Manage Allowances</a>
-                  </li>
-                  <li>
-                  <a href="/employee_allowances">Employee Allowances</a>
-                  </li>
-                  <li>
-                  <a href="/employee_allowances/create">Assign Allowances</a>
-                  </li>
-                </ul>
-              </li>
-             </li>
-                  <li class="sidebar-item has-sub">
-                <a href="#" class="sidebar-link">
-                  <i class="fa fa-building text-success"></i>
-                  <span>Deductions</span>
-                </a>
-                <ul class="submenu">
-                  <li>
-                    <a href="/deductions/create">Add Deduction</a>
-                  </li>
-                  <li>
-                    <a href="/deductions">Manage Deductions</a>
-                  </li>
-                  <li>
-                    <a href="/employee_deductions">Employee Deductions</a>
-                  </li>
-                  <li>
-                    <a href="/employee_deductions/create">Assign Deductions</a>
-                  </li>
-                </ul>
-              </li>
-             </li>
-             <li class="sidebar-item has-sub">
-                <a href="#" class="sidebar-link">
-                  <i class="fa fa-money-bill text-success"></i>
-                  <span>Payroll</span>
-                </a>
-                <ul class="submenu">
-                  <li class="sidebar-item">
-                    <a href="/payrolls/create">Generate Payroll</a>
-                  </li>
-                  <li class="sidebar-item"> 
-                    <a href="/payrolls">Manage Payroll</a>
-                  </li>
-                  <li class="sidebar-item"> 
-                    <a href="/payroll_types">Payroll Types</a>
-                  </li>
-                </ul>
-              </li>
-             {{-- <li class="sidebar-item  has-sub">
+                <li class="sidebar-item has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i class="fa fa-building text-success"></i>
+                        <span>Allowances</span>
+                    </a>
+                    <ul class="submenu">
+                        <li>
+                            <a href="/allowances/create">Add Allowance</a>
+                        </li>
+                        <li>
+                            <a href="/allowances">Manage Allowances</a>
+                        </li>
+                        <li>
+                            <a href="/employee_allowances">Employee Allowances</a>
+                        </li>
+                        <li>
+                            <a href="/employee_allowances/create">Assign Allowances</a>
+                        </li>
+                    </ul>
+                </li>
+                </li>
+                <li class="sidebar-item has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i class="fa fa-building text-success"></i>
+                        <span>Deductions</span>
+                    </a>
+                    <ul class="submenu">
+                        <li>
+                            <a href="/deductions/create">Add Deduction</a>
+                        </li>
+                        <li>
+                            <a href="/deductions">Manage Deductions</a>
+                        </li>
+                        <li>
+                            <a href="/employee_deductions">Employee Deductions</a>
+                        </li>
+                        <li>
+                            <a href="/employee_deductions/create">Assign Deductions</a>
+                        </li>
+                    </ul>
+                </li>
+                </li>
+                <li class="sidebar-item has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i class="fa fa-money-bill text-success"></i>
+                        <span>Payroll</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="sidebar-item">
+                            <a href="/payrolls/create">Generate Payroll</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="/payrolls">Manage Payroll</a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="/payroll_types">Payroll Types</a>
+                        </li>
+                    </ul>
+                </li>
+                {{-- <li class="sidebar-item  has-sub">
                 <a href="#" class='sidebar-link'>
                 <i class="fa fa-table text-success"></i>
                 <span>Leave Type</span>
@@ -211,31 +211,31 @@
                    </li>
                 </ul>
              </li> --}}
-             <li class="sidebar-item  has-sub">
-                <a href="#" class='sidebar-link'>
-                <i class="fa fa-user text-success"></i>
-                <span>Users</span>
-                </a>
-                <ul class="submenu ">
-                   <li>
-                      <a href="/register">Add User</a>
-                   </li>
-                   <li>
-                      <a href="/users">Manage Users</a>
-                   </li>
-                </ul>
-             </li>
-             <li class="sidebar-item ">
-                <a href="/reports" class='sidebar-link'>
-                <i class="fa fa-chart-bar text-success"></i>
-                <span>Reports</span>
-                </a>
-             </li>
-          </ul>
-          <div>
-            <h2>Boom</h2>
-          </div>
-       </div>
-       <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="fa fa-user text-success"></i>
+                        <span>Users</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li>
+                            <a href="/register">Add User</a>
+                        </li>
+                        <li>
+                            <a href="/users">Manage Users</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="/reports" class='sidebar-link'>
+                        <i class="fa fa-chart-bar text-success"></i>
+                        <span>Reports</span>
+                    </a>
+                </li>
+            </ul>
+            <div>
+                <h2>Boom</h2>
+            </div>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
     </div>
- </div>
+</div>
--- payroll_manager/resources/views/organizations/index.blade.php
+++ payroll_manager/resources/views/organizations/index.blade.php
@@ -1,74 +1,79 @@
 <x-layout>
-<div class="main-content container-fluid">
-                <div class="page-title">
-                    <div class="row">
-                        <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Manage Organization Info</h3>
-                        </div>
-                        <div class="col-12 col-md-6 order-md-2 order-first">
-                            <nav aria-label="breadcrumb" class='breadcrumb-header'>
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="/" class="text-success">Dashboard</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Manage Organization Info</li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
+    <div class="main-content container-fluid">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Manage Organization Info</h3>
                 </div>
-                <section class="section">
-                    <div class="card">
-                        
-                        <div class="card-body">
-                            <a type="button" class="btn btn-success mb-2" href="/organizations/create">Add Organistion Info</a>
-                            <table class='table' id="table1">
-                                <thead>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class='breadcrumb-header'>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/" class="text-success">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Manage Organization Info</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <section class="section">
+            <div class="card">
+
+                <div class="card-body">
+                    <a type="button" class="btn btn-success mb-2" href="/organizations/create">Add Organistion Info</a>
+                    <table class='table' id="table1">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Branch</th>
+                                <th>Digital Address</th>
+                                <th>City</th>
+                                <th>Region</th>
+                                <th>Contact</th>
+                                <th>Email</th>
+                                <th>Website</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @unless (count($organizations) == 0)
+                                @foreach ($organizations as $organization)
                                     <tr>
-                                        <th>Organization Name</th>
-                                        <th>Organization Branch</th>
-                                        <th>Digital Address</th>
-                                        <th>City</th>
-                                        <th>Organization Region</th>
-                                        <th>Organization Contact</th>
-                                        <th>Organization Email</th>
-                                        <th>Organization Website</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                     @unless(count($organizations) == 0)
-                                     @foreach ($organizations as $organization)
-                                    <tr>
-                                        <td>{{$organization->organization_name}}</td>
-                                        <td>{{$organization->organization_branch}}</td>
-                                        <td>{{$organization->digital_address}}</td>
-                                        <td>{{$organization->organization_city}}</td>
-                                        <td>{{$organization->organization_region}}</td>
-                                        <td>{{$organization->organization_contact}}</td>
-                                        <td>{{$organization->organization_email}}</td>
-                                        <td>{{$organization->organization_website}}</td>
-                                        <td>{{$organization->created_at}}</td>
-                                        <td class="d-flex"><a type="button" class="btn btn-info me-1 mb-1" href="/organizations/{{$organization->id}}/edit"><i class="fa fa-pen"></i></a>   
-                                            <form action="/organizations/{{$organization->id}}" method="POST">
+                                        <td>{{ $organization->organization_name }}</td>
+                                        <td>{{ $organization->organization_branch }}</td>
+                                        <td>{{ $organization->digital_address }}</td>
+                                        <td>{{ $organization->organization_city }}</td>
+                                        <td>{{ $organization->organization_region }}</td>
+                                        <td>{{ $organization->organization_contact }}</td>
+                                        <td>{{ $organization->organization_email }}</td>
+                                        <td>{{ $organization->organization_website }}</td>
+                                        <td class="d-flex"><a type="button" class="btn btn-info me-1 mb-1"
+                                                href="/organizations/{{ $organization->id }}/edit"><i
+                                                    class="fa fa-pen"></i></a>
+                                            <form action="/organizations/{{ $organization->id }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
-                                              
-                                            &nbsp;
-                                            <button type="submit" class="btn btn-danger me-1 mb-1" onclick="return confirm('{{ __('Are you sure you want to delete?') }}')"><i class="fa fa-trash"></i></button>
-                                        </form>
-                                        </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                    @else
-                                    <tr>
-                                        <td colspan="4"><p class="text-center">No Info found</p></td>
-                                    </tr>
-                                    @endunless
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
 
-                </section>
+                                                &nbsp;
+                                                <button type="submit" class="btn btn-danger me-1 mb-1"
+                                                    onclick="return confirm('{{ __('Are you sure you want to delete?') }}')"><i
+                                                        class="fa fa-trash"></i></button>
+                                            </form>
+                    </div>
+                    </td>
+                    </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="4">
+                            <p class="text-center">No Info found</p>
+                        </td>
+                    </tr>
+                @endunless
+                </tbody>
+                </table>
             </div>
+    </div>
+
+    </section>
+    </div>
 </x-layout>
